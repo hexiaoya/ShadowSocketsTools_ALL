a37d35553e4f854532e43f43c2bd2ebea0614a43
hexiaoya <1175708335@qq.com>
2016-11-22 17:45:29 +0800

    sixth commit

---
+++ ShadowSocks-server/shadowsocks-server.bat
@@ -0,0 +1,5 @@
+::This batch will run shadowsocks-libqss in server mode using the config.json file in current folder as the configuration
+
+@echo off
+::this script is updated for version 1.7.0
+shadowsocks-libqss.exe -c config.json -S
